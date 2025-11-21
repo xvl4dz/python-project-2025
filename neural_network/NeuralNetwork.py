9b81a783ae999314a4f858d35a2788fb83bd6778
xvl4dz <zarytov.vp@phystech.org>
2025-11-22 01:33:02 +0300

    implemented NeuralNetwork.load_model

--- neural_network/NeuralNetwork.py
+++ neural_network/NeuralNetwork.py
@@ -1,4 +1,5 @@
 import numpy as np
+import pickle
 from .Layer import Layer
 from .Cost import Cost
 
@@ -141,3 +142,31 @@ class NeuralNetwork:
             pickle.dump(model_data, f)
         
         print(f"Model saved to {filepath}")
+
+    @classmethod
+    def load_model(cls, filepath):
+        """Load model from file and recreate the neural network"""
+        with open(filepath, 'rb') as f:
+            model_data = pickle.load(f)
+        
+        # Extract architecture and parameters
+        arch = model_data['architecture']
+        parameters = model_data['parameters']
+        
+        # Create new neural network with the same architecture
+        model = cls(
+            input_size=arch['input_size'],
+            hidden_layers=arch['hidden_layers'],
+            output_size=arch['output_size'],
+            learning_rate=arch['learning_rate'],
+            activation=arch['activation'],
+            cost_function=arch['cost_function']
+        )
+        
+        # Set the loaded parameters
+        model.set_parameters(parameters)
+        
+        print(f"Model loaded from {filepath}")
+        print(f"Architecture: {arch['input_size']} -> {arch['hidden_layers']} -> {arch['output_size']}")
+        
+        return model
